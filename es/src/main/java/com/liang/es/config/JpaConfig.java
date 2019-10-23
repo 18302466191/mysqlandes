c22fae4a607b414bad87de60062b0ca1e65f82c9
18302466191 <liangbiyin@163.com>
2019-10-23 17:34:45 +0800

    梁碧崟修改了代码

---
+++ es/src/main/java/com/liang/es/config/JpaConfig.java
@@ -0,0 +1,49 @@
+package com.liang.es.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+/*
+* jpa配置类
+* 2019-10-23
+* LiangBiyin
+* */
+@Configuration
+@EnableJpaRepositories(basePackages ="com.liang.es.repositiory")
+@EnableTransactionManagement
+public class JpaConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+        HibernateJpaVendorAdapter jpaVendor =new HibernateJpaVendorAdapter();
+        jpaVendor.setGenerateDdl(false);
+        jpaVendor.setShowSql(true);
+        jpaVendor.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean=new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendor);
+        entityManagerFactoryBean.setPackagesToScan("com.liang.es.entity");
+        return entityManagerFactoryBean;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager transactionManager=new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return  transactionManager;
+    }
+}
